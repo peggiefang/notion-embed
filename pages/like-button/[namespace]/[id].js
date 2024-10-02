f4fd5ad4763c032166fa65b4062a62416b1a418c
Peggie Fang <77263194+peggiefang@users.noreply.github.com>
2024-10-02 21:02:20 +0800

    Update [id].js

--- pages/like-button/[namespace]/[id].js
+++ pages/like-button/[namespace]/[id].js
@@ -1,9 +1,27 @@
-import { LikeButton } from "@lyket/react";
-import { useRouter } from "next/router";
+// import { LikeButton } from "@lyket/react";
+// import { useRouter } from "next/router";
 
-export default function Button() {
-  const router = useRouter();
-  const { namespace, id } = router.query;
+// export default function Button() {
+//   const router = useRouter();
+//   const { namespace, id } = router.query;
 
-  return <LikeButton namespace={namespace} id={id} />;
-}
+//   return <LikeButton namespace={namespace} id={id} />;
+// }
+
+import { Provider, LikeButton } from "@lyket/react";
+
+<Provider
+  apiKey="acc0dbccce8e557db5ebbe6d605aaa"
+  theme={{
+    colors: {
+      background: "#b8fff3",
+      text: "violet",
+      primary: "rgba(255, 224, 138, 0.4)"
+    }
+  }}
+>
+  <LikeButton
+    namespace="my-blog-post"
+    id="how-to-beat-me-at-chess"
+  />
+</Provider>
