b9ef65f5b9fc666d8e6e510bfd55effa5898a818
Peggie Fang <77263194+peggiefang@users.noreply.github.com>
2024-10-02 21:21:37 +0800

    Delete pages/clap.js

--- pages/clap.js
+++
@@ -1,8 +0,0 @@
-import { Provider, ClapButton } from "@lyket/react";
-
-<Provider apiKey="pt_762578bb9c5fba2e90b9f936e9f8cc">
-  <ClapButton
-    namespace="testing-react"
-    id="everybody-clap-now"
-  />
-</Provider>
