7757de1743b955ae5051899b8a7c7609948ed17d
Peggie Fang <77263194+peggiefang@users.noreply.github.com>
2024-10-02 21:09:50 +0800

    Update [id].js

--- pages/like-button/[namespace]/[id].js
+++ pages/like-button/[namespace]/[id].js
@@ -7,21 +7,3 @@ export default function Button() {
 
   return <LikeButton namespace={namespace} id={id} />;
 }
-
-// import { Provider, LikeButton } from "@lyket/react";
-
-// <Provider
-//   apiKey="pt_762578bb9c5fba2e90b9f936e9f8cc"
-//   theme={{
-//     colors: {
-//       background: "#b8fff3",
-//       text: "violet",
-//       primary: "rgba(255, 224, 138, 0.4)"
-//     }
-//   }}
-// >
-//   <LikeButton
-//     namespace="my-blog-post"
-//     id="how-to-beat-me-at-chess"
-//   />
-// </Provider>
