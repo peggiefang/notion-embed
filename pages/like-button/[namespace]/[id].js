69d6a8dc64e4b1d827bf4b015aa3e2094e13d8aa
Peggie Fang <77263194+peggiefang@users.noreply.github.com>
2024-10-02 21:07:09 +0800

    Update [id].js

--- pages/like-button/[namespace]/[id].js
+++ pages/like-button/[namespace]/[id].js
@@ -1,27 +1,40 @@
-// import { LikeButton } from "@lyket/react";
-// import { useRouter } from "next/router";
+import { LikeButton } from "@lyket/react";
+import { useRouter } from "next/router";
 
-// export default function Button() {
-//   const router = useRouter();
-//   const { namespace, id } = router.query;
+export default function Button() {
+  const router = useRouter();
+  const { namespace, id } = router.query;
+  const defaultTheme = {
+  colors: {
+    primary: '#22c1c3',
+    secondary: '#ff00c3',
+    background: 'transparent',
+    text: '#292929',
+    highlight: '#e095ed',
+    icon: '#292929',
+  },
+  fonts: {
+    body: 'inherit',
+  },
+};
 
-//   return <LikeButton namespace={namespace} id={id} />;
-// }
+  return <LikeButton namespace={namespace} id={id} />;
+}
 
-import { Provider, LikeButton } from "@lyket/react";
+// import { Provider, LikeButton } from "@lyket/react";
 
-<Provider
-  apiKey="pt_762578bb9c5fba2e90b9f936e9f8cc"
-  theme={{
-    colors: {
-      background: "#b8fff3",
-      text: "violet",
-      primary: "rgba(255, 224, 138, 0.4)"
-    }
-  }}
->
-  <LikeButton
-    namespace="my-blog-post"
-    id="how-to-beat-me-at-chess"
-  />
-</Provider>
+// <Provider
+//   apiKey="pt_762578bb9c5fba2e90b9f936e9f8cc"
+//   theme={{
+//     colors: {
+//       background: "#b8fff3",
+//       text: "violet",
+//       primary: "rgba(255, 224, 138, 0.4)"
+//     }
+//   }}
+// >
+//   <LikeButton
+//     namespace="my-blog-post"
+//     id="how-to-beat-me-at-chess"
+//   />
+// </Provider>
